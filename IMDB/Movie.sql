643b840be57c089c5c13e7a665273f5f90a70ff9
stevebstorm <steve.lorent@gmail.Com>
2024-04-03 16:47:35 +0200

    ajout projet DB + Data

---
+++ IMDB/Movie.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Movie]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	Title VARCHAR(100) NOT NULL,
+	Description VARCHAR(500),
+	RealisatorId INT
+
+	CONSTRAINT FK_Movie_Real FOREIGN KEY (RealisatorId) REFERENCES Person(Id)
+)
---
+++ IMDB/Script.PostDeployment1.sql
@@ -0,0 +1,30 @@
+﻿/*
+Modèle de script de post-déploiement							
+--------------------------------------------------------------------------------------
+ Ce fichier contient des instructions SQL qui seront ajoutées au script de compilation.		
+ Utilisez la syntaxe SQLCMD pour inclure un fichier dans le script de post-déploiement.			
+ Exemple :      :r .\monfichier.sql								
+ Utilisez la syntaxe SQLCMD pour référencer une variable dans le script de post-déploiement.		
+ Exemple :      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+INSERT INTO Person VALUES ('Hammil', 'Mark', 'https://resize-elle.ladmedia.fr/rcrop/638,,forcex/img/var/plain_site/storage/images/loisirs/cinema/dossiers/que-sont-ils-devenus-les-acteurs-de-star-wars/mark-hamill2/92295653-1-fre-FR/Mark-Hamill.jpg')
+INSERT INTO Person VALUES ('Ford', 'Harisson', 'https://m.media-amazon.com/images/M/MV5BMTY4Mjg0NjIxOV5BMl5BanBnXkFtZTcwMTM2NTI3MQ@@._V1_FMjpg_UX1000_.jpg')
+INSERT INTO Person VALUES ('Fisher', 'Carrie', 'https://resize.elle.fr/portrait/var/plain_site/storage/images/people/la-vie-des-people/news/carrie-fisher-le-message-d-outre-tombe-de-la-princesse-leia-qui-a-emu-sa-famille-3829301/92306648-1-fre-FR/Carrie-Fisher-le-message-d-outre-tombe-de-la-princesse-Leia-qui-a-emu-sa-famille.jpg')
+INSERT INTO Person VALUES ('Lucas', 'George', 'https://fr.web.img6.acsta.net/pictures/15/12/18/10/51/568937.jpg')
+INSERT INTO Person VALUES ('Spielberg', 'Steven', 'https://fr.web.img5.acsta.net/pictures/16/05/17/11/39/453609.jpg')
+
+
+INSERT INTO Movie VALUES ('Star Wars - A New Hope', 4)
+INSERT INTO Movie VALUES ('Star Wars - Empire Strikes Back', 4)
+INSERT INTO Movie VALUES ('Indiana Jones - Cursed temple', 5)
+
+INSERT INTO Movie_Person VALUES (1, 1, 'Luke Skywalker')
+INSERT INTO Movie_Person VALUES (2, 1, 'Han Solo')
+INSERT INTO Movie_Person VALUES (3, 1, 'Leia Organa')
+INSERT INTO Movie_Person VALUES (1, 2, 'Luke Skywalker')
+INSERT INTO Movie_Person VALUES (2, 2, 'Han Solo')
+INSERT INTO Movie_Person VALUES (3, 2, 'Leia Organa')
+INSERT INTO Movie_Person VALUES (2, 3, 'Indiana Jones')
